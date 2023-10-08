5f52d763b2c1e71f49b192e695da7b7a485479c0
alighn <alighndev@protonmail.com>
2023-10-08 19:20:44 +0330

    Added configuration_exceptions.py - added documentation - added exception handling

--- configuration/configuration.py
+++ configuration/configuration.py
@@ -1,19 +1,33 @@
 from inspect import getmembers_static, isroutine
 from json import load, dump
 import re
-
-import attr
+from exceptions.configuration_exceptions import ConfigurationExceptions
 
 
 class ConfigurationHelper:
     def __init__(self, configuration_file_location: str):
+        """
+        Configuration Helper will enable dependency-injection in a very convenient way.
+        :param configuration_file_location: hence the name it requires configuration file location
+        """
         self.configuration_file_location = configuration_file_location
 
     def __int__(self):
+        """
+        If configuration helper is not required to handle dependency-injection it does not require the path but
+        all the methods are static so there is no need for such an initialization.
+        :return:
+        """
         self.configuration_file_location = None
 
     @staticmethod
     def get_configurations(target_class, configuration_file_location: str) -> dict:
+        """
+        get_configuration will load the config from a file using the blueprint that target_class has provided.
+        :param target_class: the class which configs should be loaded from their static attributes.
+        :param configuration_file_location: hence the name it requires configuration file location.
+        :return:
+        """
         members = getmembers_static(target_class)
         properties = list()
         for member in members:
@@ -25,6 +39,14 @@ class ConfigurationHelper:
 
     @staticmethod
     def save_configurations(target_class, configuration_file_location: str, **kwargs):
+        """
+        save_configuration will save the configs which has been provided using `**kwargs` parameter to the
+        `configuration_file_location` parameter's value.
+        :param target_class: blueprint of the config.
+        :param configuration_file_location: hence the name it requires configuration file location.
+        :param kwargs: configs which should meet target_class's blueprint of their attributes.
+        :return:
+        """
         attributes = ConfigurationHelper.get_class_attributes(target_class)
         configs = dict()
         for key in kwargs:
@@ -35,6 +57,11 @@ class ConfigurationHelper:
 
     @staticmethod
     def get_class_attributes(target_class) -> dict:
+        """
+        get_class_attributes will fetch all the required attributes for the configuration as the name suggests.
+        :param target_class: the class which it has the blueprints of attributes.
+        :return:
+        """
         members = getmembers_static(target_class)
         properties = dict()
         for member, value in members:
@@ -52,6 +79,8 @@ class ConfigurationHelper:
         :param config_class: class which has configuration attributes
         :return: target_class which is instantiated with the config
         """
+        if self.configuration_file_location is None:
+            raise ConfigurationExceptions.ConfigurationFileIsNotInitialized
         try:
             configs = ConfigurationHelper.get_configurations(config_class, self.configuration_file_location)
             sanitized_configs = dict()
---
+++ exceptions/configuration_exceptions.py
@@ -0,0 +1,3 @@
+
+class ConfigurationExceptions:
+    ConfigurationFileIsNotInitialized = Exception("Configuration file location has not been initialized in the __init__")
